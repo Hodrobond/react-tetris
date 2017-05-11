c994a4842ea2434014f1c410b5e33d90d673e8a1
Adam Kazberuk <hodrobond@gmail.com>
2017-05-11 09:15:29 -0700

    fixed bug not clearing rows that were not on the edge, added test

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -33,17 +33,18 @@ function addPiece(state, piece, rotation, position){
 function clearRows(state1){
   var state = _.cloneDeep(state1);
   var length = state[state.length - 1].length
-  for(var i = 0; i < length; i++){
-    if(state[state.length - 1][i] === false){
-      break;
-    }
-    if(i === length - 1){
-      state.splice(state.length - 1, 1);
-      state.unshift(new Array(state[0].length));
-      for(var j = 0; j < state[0].length; j++){
-        state[0][j] = false;
+  for(var i = 0; i < state.length; i++){
+    for(var j = 0; j < state[i].length; j++){
+      if(state[i][j] === false){
+        break;
       }
-      state = clearRows(state);
+      if(j === state[i].length - 1){
+        state.splice(i, 1);
+        state.unshift(new Array(state[0].length));
+        for(var j = 0; j < state[0].length; j++){
+          state[0][j] = false;
+        }
+      }
     }
   }
   return state;
--- src/reducers/board.test.js
+++ src/reducers/board.test.js
@@ -64,3 +64,22 @@ it('Clear rows - 4', () => {
   let newBoard = Board(state, {type: "CLEAR_ROWS"});
   expect(newBoard).toEqual(solution);
 })
+
+it('Clear rows - non-edge', () => {
+  let state = new Array(20);
+  let solution = new Array(20);
+  for(var i = 0; i < 20; i++){
+    state[i] = new Array(10);
+    solution[i] = new Array(10);
+    for(var j = 0; j < 10; j++){
+      if(i == 18)
+        state[i][j] = true;
+      else
+        state[i][j] = false;
+
+      solution[i][j] = false;
+    }
+  }
+  let newBoard = Board(state, {type: "CLEAR_ROWS"});
+  expect(newBoard).toEqual(solution);
+})
