487bec407e3dcb9a633d2ecb7db215474ccc8c1c
Adam Kazberuk <hodrobond@gmail.com>
2017-05-11 08:41:05 -0700

    Test case for multiple rows

--- src/reducers/board.test.js
+++ src/reducers/board.test.js
@@ -43,5 +43,24 @@ it('Clear rows - none', () => {
     }
   }
   let newBoard = Board(state, {type: "CLEAR_ROWS"});
-  expect(newBoard).toEqual(solution);  
+  expect(newBoard).toEqual(solution);
+})
+
+it('Clear rows - 4', () => {
+  let state = new Array(20);
+  let solution = new Array(20);
+  for(var i = 0; i < 20; i++){
+    state[i] = new Array(10);
+    solution[i] = new Array(10);
+    for(var j = 0; j < 10; j++){
+      if(i > 15)
+        state[i][j] = true;
+      else
+        state[i][j] = false;
+
+      solution[i][j] = false;
+    }
+  }
+  let newBoard = Board(state, {type: "CLEAR_ROWS"});
+  expect(newBoard).toEqual(solution);
 })
