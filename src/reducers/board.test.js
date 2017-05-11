cc7ae6a83ead46282fc84a69be11f60d7e93fdab
Adam Kazberuk <hodrobond@gmail.com>
2017-05-11 09:44:27 -0700

    Adding a test for adding a piece to the board

--- src/reducers/board.test.js
+++ src/reducers/board.test.js
@@ -1,7 +1,9 @@
 import Board from './board'
 
+var PieceTypes = require('../modules/pieces');
+
 it('Initialize', () => {
-  const initialized = Board(0, {type:'NEW_GAME'});
+  const initialized = Board(0);
   let solution = new Array(20);
   for(var i = 0; i < 20; i++){
     solution[i] = new Array(10);
@@ -12,7 +14,7 @@ it('Initialize', () => {
   expect(initialized).toEqual(solution);
 })
 
-it('Clear rows - 1', () => {
+it('Clear rows - 1 bottom', () => {
   let state = new Array(20);
   let solution = new Array(20);
   for(var i = 0; i < 20; i++){
@@ -46,7 +48,7 @@ it('Clear rows - none', () => {
   expect(newBoard).toEqual(solution);
 })
 
-it('Clear rows - 4', () => {
+it('Clear rows - 4 bottom', () => {
   let state = new Array(20);
   let solution = new Array(20);
   for(var i = 0; i < 20; i++){
@@ -65,7 +67,7 @@ it('Clear rows - 4', () => {
   expect(newBoard).toEqual(solution);
 })
 
-it('Clear rows - non-edge', () => {
+it('Clear rows - 1 non-edge', () => {
   let state = new Array(20);
   let solution = new Array(20);
   for(var i = 0; i < 20; i++){
@@ -82,4 +84,34 @@ it('Clear rows - non-edge', () => {
   }
   let newBoard = Board(state, {type: "CLEAR_ROWS"});
   expect(newBoard).toEqual(solution);
+})
+
+it('Add piece', () => {
+  let state = new Array(20);
+  let solution = new Array(20);
+  for(var i = 0; i < 20; i++){
+    state[i] = new Array(10);
+    solution[i] = new Array(10);
+    for(var j = 0; j < 10; j++){
+      state[i][j] = false;
+      solution[i][j] = false;
+    }
+  }
+  const testPiece = {
+    _piece: PieceTypes.I,
+    _rotation: 0,
+    _position: {
+      x: 4,
+      y: 15
+    }
+  }
+
+  solution[15][5] = "piece-i";
+  solution[16][5] = "piece-i";
+  solution[17][5] = "piece-i";
+  solution[18][5] = "piece-i";
+
+  let newBoard = Board(state, {type: "ADD_PIECE", piece: testPiece._piece, rotation: testPiece._rotation, position: testPiece._position});
+  expect(newBoard).toEqual(solution);
+
 })
