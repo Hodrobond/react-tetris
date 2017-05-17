73a353e0ddb32c87a2d160d48ef0647f37376fbf
Adam Kazberuk <hodrobond@gmail.com>
2017-05-17 09:15:25 -0700

    some tests

---
+++ src/utility/board.test.js
@@ -0,0 +1,105 @@
+import {getPreviewPosition, isEmptyPosition} from './board'
+
+var emptyBoard = [[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0]];
+
+var testBoard1 = [[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[1,1,1,1,1,1,1,1,1,0]]
+
+const testPiece = {
+  _piece:{
+    blocks: [
+      [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
+      ],
+      [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0]
+      ],
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0]
+      ]
+    ],
+    className: 'piece-i'
+  },
+  _position: {
+    x: 3,
+    y: 2
+  },
+  _rotation: 0
+};
+
+it('Empty Position Yes - 1', () => {
+  var empty = isEmptyPosition(emptyBoard, testPiece._piece, testPiece._rotation, testPiece._position);
+  const solution = true;
+  expect(empty).toEqual(solution);
+})
+
+it('Empty Position No - 1', () => {
+  var empty = isEmptyPosition(emptyBoard, testPiece._piece, testPiece._rotation, {x: testPiece._position.x, y: 19});
+  const solution = false;
+  expect(empty).toEqual(solution);
+})
+
+it('Empty Position + Preview Yes - 1', () => {
+  var preview = getPreviewPosition(emptyBoard, testPiece._piece, testPiece._rotation, testPiece._position);
+  var empty = isEmptyPosition(emptyBoard, testPiece._piece, testPiece._rotation, {x: testPiece._position.x, y: preview});
+  const solution = true;
+  expect(empty).toEqual(solution);
+})
+
+it('Get Preview - 1', () => {
+  var preview = getPreviewPosition(emptyBoard, testPiece._piece, testPiece._rotation, testPiece._position);
+  const solution = 16;
+  expect(preview).toEqual(solution);
+})
